d0e4f3f76c9ad814d6007b5b17545a6da0ad5a1d
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-07-02 18:26:57 -0700

    changed file and folders

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainBoard extends React.Component {
+	constructor(props) {
+	
+	}
+	
+	render () {
+		return (
+			<div> Hello World! </div>
+		)
+	}
+}
+
+export default MainPage;
