2659f0d234154a99cb8f0126cf016df1d54b6204
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-07-09 14:23:44 -0700

    added some comments and TODO, and created JSON file to store recepies for now

--- src/components/pages/DetailRecipePage.jsx
+++ src/components/pages/DetailRecipePage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 class DetailRecipePage extends React.Component {
-	
+	//TODO:
+	// * Find how to get ID from the URL
+	// https://localhost:3000/DetailRecipePage?id=1 or
+	// https://localhost:3000/DetailRecipePage/1 or so that you user https://stackoverflow.com/a/45599016
+
 	render() {
 	  return (
 	  	<div>
 			<h2>Here is your rercipe in detail</h2>
+			{/* <h3>THis is recipe #{id}</h3> */}
 		</div>
 	  )
 	}
