ebf36f51f4d4ff202ed19016976e45e01736b818
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-07-21 16:21:16 -0700

    working around template format and display

--- src/components/pages/RecipeTemplate.jsx
+++ src/components/pages/RecipeTemplate.jsx
@@ -4,15 +4,47 @@ import data1 from '../../mock/data1.json';
 
 class RecipeTemplate extends React.Component {
 	render () {
+		let ingredient = JSON.stringify(data1.ingredients);
+		let s = '';
+		let a = [];
+		for (let i = 0; i < 10; i++) {
+			s += `<u> Hi </u> + ${i}`;
+			a.push(i);
+		}
 		return (
 			<div>
 				<table key={ data1.id }>
 					<tr>
-						<td>{ data1.title }</td>
-						<td>{ JSON.stringify(data1.ingredients) }</td>
-						<td>{ data1.calories }</td>
-						<td>{ data1.instruction }</td>
-						<td>{ data1.tags }</td>
+						<tr>
+							<th>
+								{ data1.title } and { s }
+								<br /> 
+								{ 
+									a.map((x) => (
+										<div>
+											<s>{x}</s> <hr />
+										</div> 
+									))
+								}
+							</th>
+						</tr>
+
+						<tr>
+							<td>{ ingredient }</td>
+						</tr>
+
+						<tr>
+							<td>{ data1.calories }</td>
+						</tr>
+
+						<tr>
+							<td>{ data1.instruction }</td>
+						</tr>
+
+						<tr>
+							<td>{ data1.tags }</td>
+						</tr>
+
 						<a href={ data1.imgURL }>Photo</a>
 					</tr>
 				</table>
