8cc8234b9d5cea51d6343677d8cee20003e87edc
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-07-22 14:41:00 -0700

    used RecipeTemplate and passed data to it dynamically

--- src/components/pages/DetailRecipePage.jsx
+++ src/components/pages/DetailRecipePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import RecipeTemplate from './RecipeTemplate';
-import data1 from '../../mock/data1.json';
+import { Link } from 'react-router-dom';
 
 class DetailRecipePage extends React.Component {
 	constructor(props) {
@@ -9,12 +9,13 @@ class DetailRecipePage extends React.Component {
 
 	render() {
 		const id = this.props.match.params.id;
-		console.log({ gholi: this.props });
+		// console.log({ gholi: this.props });
 
 	  return (
 	  	<div className="detail-recipe-page" >
-			<h2>Here is your rercipe { id } in detail </h2>
-			<RecipeTemplate recipeData={this.props.recipeData}/>
+			<Link to="/show-all-recipes">&lt; Back</Link>
+			{/* <h2>Here is your rercipe { id } in detail </h2> */}
+			<RecipeTemplate id={ id } />
 		</div>
 	  )
 	  //TODO: use recipe template here and pass "data" to it as a prop
--- src/components/pages/RecipeTemplate.jsx
+++ src/components/pages/RecipeTemplate.jsx
@@ -1,53 +1,86 @@
 import React from 'react';
-import data1 from '../../mock/data1.json';
-
 
 class RecipeTemplate extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+		// TODO: axios.get("backend/getRecipeData", { id: this.props.id })
+		import(`../../mock/data${this.props.id}.json`)
+			.then((jsonData) => {
+				this.setState({
+					...this.state,
+					content: jsonData
+				});
+			});
+	}
+
 	render () {
-		let ingredient = JSON.stringify(data1.ingredients);
-		let s = '';
-		let a = [];
-		for (let i = 0; i < 10; i++) {
-			s += `<u> Hi </u> + ${i}`;
-			a.push(i);
-		}
+		// let ingredient = JSON.stringify(data1.ingredients);
+		// let s = '';
+		// let a = [];
+		// for (let i = 0; i < data1.ingredients.length; i++) {
+		// 		s += ` ${i}`;
+		// 		a.push(ingredients[i]);
+		// }
+
 		return (
 			<div>
-				<table key={ data1.id }>
-					<tr>
+				{ !this.state.content ? (
+					<h2>loading</h2>
+				) : (
+					<table key={ this.state.content.id }>
+					<tbody>
 						<tr>
 							<th>
-								{ data1.title } and { s }
-								<br /> 
-								{ 
-									a.map((x) => (
-										<div>
-											<s>{x}</s> <hr />
-										</div> 
-									))
-								}
+								{ this.state.content.title } 
+								
 							</th>
 						</tr>
 
 						<tr>
-							<td>{ ingredient }</td>
+							<td> 
+								<h5>Ingredients</h5>
+								<br /> 
+								{ 
+									this.state.content.ingredients.map((ingredient, i) => (
+										<div key={i}>
+											{/* 	<s>{x}</s> <hr /> */}
+											{ingredient.name + "-" + ingredient.quantity} 
+										</div> 
+									))
+								} 
+							</td>
 						</tr>
 
 						<tr>
-							<td>{ data1.calories }</td>
+							<td> 
+								<h5>Calorie</h5> 
+								{ this.state.content.calories }
+							</td>
 						</tr>
 
 						<tr>
-							<td>{ data1.instruction }</td>
+							<td> 
+								<h5>Instruction:</h5> 
+								{ this.state.content.instruction }
+							</td>
 						</tr>
 
 						<tr>
-							<td>{ data1.tags }</td>
+							<td> 
+								<h5>Tags</h5> 
+								{ this.state.content.tags }
+							</td>
 						</tr>
 
-						<a href={ data1.imgURL }>Photo</a>
-					</tr>
+						<tr>
+							<td>
+								<img src={ this.state.content.imgURL } />
+							</td>
+						</tr>
+					</tbody>
 				</table>
+				)}
 			</div>
 		)
 	}
