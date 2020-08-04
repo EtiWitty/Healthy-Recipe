8ce94ddb704218ea2ce7f647fd439c1d8071c4fa
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-08-04 15:21:28 -0700

    modified mongoose schema

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -2,36 +2,48 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
+	_id: mongoose.Schema.ObjectId,
 	name: {
 		type: String,
 		// any space in the beginning or end will be trimmed
 		trim: true, 
 		required: true,
 		maxLength: 32
-	  },
-	  ingredients: {
-		type: String,// array? 
-		trim: true, 
+	},
+	ingredients: {
+		type: Array,
 		required: true,
-	  },
-	  calories: {
+		detail: [{ 
+			name: {
+				type: String,
+				trim: true, 
+				required: true,
+			},
+			quantity: {
+			  type: Number, 
+			  trim: true, 
+			  required: true,
+			}
+		}]
+	},
+	calories: {
 		type: Number,
 		required: true,
-	  },
-	  imgURL: {
+	},
+	imgURL: {
 		type: String,
 		required: true,
 		unique: true
-	  },
-	  instructions: {
+	},
+	instructions: {
 		type: String,
 		trim: true, 
 		required: true,
-	  },
-	  tags: {
+	},
+	tags: {
 		type: Array,
 		required: true,
-	  },
+	}	
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
