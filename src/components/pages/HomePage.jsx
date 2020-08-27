9b499525c1e7ac738d304edc186be8e3991fb755
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-08-27 16:13:22 -0700

    fixed drop down button

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -7,7 +7,6 @@ class HomePage extends React.Component {
 	render() {
 		return (
 			<div className="back-image" style={{backgroundImage: `url(${homeImg})`}}> 
-			{/* <svg></svg> ????*/}
 				<Search />
 			</div>
 		);
