29edf180b3461bd04745585cc4a57bdedcfc7e47
etiwitty <atiyeh.keshavarz@gmail.com>
2020-10-23 17:40:51 -0700

    initiated Admin page

---
+++ src/components/Admin.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Admin extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4>Admin Portal</h4>
+            </div>
+        )
+    }
+}
+
+export default Admin;
+
+/*
+
+Different Inputs:
+
+Title
+Calories
+imgUrl
+Instruction
+Ingredients
+Tags
+
+*/
