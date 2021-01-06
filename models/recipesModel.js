fbd443269e04c9e0055af4bc3ab157614fcfb1f3
etiwitty <atiyeh.keshavarz@gmail.com>
2021-01-05 16:05:37 -0800

    revert the change so we can see ingredients in detail recipe page again

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -1,58 +1,61 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		// any space in the beginning or end will be trimmed
-		trim: true, 
-		required: true,
-		maxLength: 32
-	},
+  title: {
+    type: String,
+    // any space in the beginning or end will be trimmed
+    trim: true,
+    required: true,
+    maxLength: 32,
+  },
 
-	// ingredients: [{ 
-	// 	name: {
-	// 		type: String,
-	// 		trim: true, 
-	// 		required: true,
-	// 	},
-	// 	quantity: {
-	// 		type: Number, 
-	// 		trim: true, 
-	// 		required: true,
-	// 	}
-	// }],
+  ingredients: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        trim: true,
+        required: true,
+      },
+    },
+  ],
 
-	ingredients: [{ 
-		type: String,
-		trim: true, 
-		required: true,
-	}],
+  // ingredients: [
+  //   {
+  //     type: String,
+  //     trim: true,
+  //     required: true,
+  //   },
+  // ],
 
-	quantity: {
-		type: Number, 
-		trim: true, 
-		required: true,
-	},
-	
-	calories: {
-		type: Number,
-		required: true,
-	},
-	imgURL: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	instruction: {
-		type: String,
-		trim: true, 
-		required: true,
-	},
-	tags: {
-		type: Array,
-		required: true,
-	}	
+  // quantity: {
+  //   type: Number,
+  //   trim: true,
+  //   required: true,
+  // },
+
+  calories: {
+    type: Number,
+    required: true,
+  },
+  imgURL: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  instruction: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  tags: {
+    type: Array,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Recipe', recipeSchema);
+module.exports = mongoose.model("Recipe", recipeSchema);
